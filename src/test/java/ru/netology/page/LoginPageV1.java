02cc871c5bc04d8beea242ae50aa585814723414
Artyel <artyel@icloud.com>
2023-06-15 08:47:22 +0100

    Tests added

--- src/test/java/ru/netology/page/LoginPageV1.java
+++ src/test/java/ru/netology/page/LoginPageV1.java
@@ -1,15 +1,22 @@
 package ru.netology.page;
 
+import com.codeborne.selenide.SelenideElement;
 import ru.netology.data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPageV1 {
 
-    public VerificationPage validLogin (DataHelper.AuthInfo info) {
-        $("[data-test-id=login] input").setValue(info.getLogin());
-        $("[data-test-id=password] input").setValue(info.getPassword());
-        $("[data-test-id=action-login]").click();
+    private static final SelenideElement loginField = $ ("[data-test-id=login] input");
+    private static final SelenideElement passwordField = $("[data-test-id=password] input");
+    private static final SelenideElement loginButton = $("[data-test-id=action-login]");
+
+
+
+    public static VerificationPage validLogin(DataHelper.AuthInfo info) {
+      loginField.setValue(info.getLogin());
+      passwordField.setValue(info.getPassword());
+      loginButton.click();
         return new VerificationPage();
     }
 }
